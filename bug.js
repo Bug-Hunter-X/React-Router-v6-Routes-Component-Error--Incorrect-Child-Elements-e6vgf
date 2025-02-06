e488d7f04936cb9c3a3a4a8a911e52b188ac5191
BugHunter-X <askzenai@gmail.com>
2025-02-06 12:26:40 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,24 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* ... other routes */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <div>Home</div>;
+}
+
+function About() {
+  return <div>About</div>;
+}
+```
+This code causes an error because of incorrect usage of `Routes` component in react-router-dom v6. `Routes` should only contain `Route` components as children.  If you have a nested route structure, you would nest `Routes` components.
